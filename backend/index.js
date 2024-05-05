2684b2ee6d08b146b3c986637be8b3b567ac3bff
AnujYadav2003 <foranujyadav@gmail.com>
2024-05-05 18:14:08 +0530

    added index

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 
 const express = require("express");
+
+require('dotenv').config()
 const mainRouter = require('./routes/index');
 
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+const PORT=3000;
 app.use(cors());
 app.use(bodyParser.json()); 
 
@@ -15,6 +17,6 @@ app.use("/api/v1", mainRouter);
 
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("listening on port 3000");
 });
